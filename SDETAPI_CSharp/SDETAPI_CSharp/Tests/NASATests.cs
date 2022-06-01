2fbc34680995753882bbbce319e38d1d8c43ba74
carlos.martinez <carlos.martinez@REM-LT-220516B>
2022-06-01 13:13:39 -0500

    Third Pull Request

--- SDETAPI_CSharp/SDETAPI_CSharp/Tests/NASATests.cs
+++ SDETAPI_CSharp/SDETAPI_CSharp/Tests/NASATests.cs
@@ -1,41 +1,23 @@
 ﻿using Newtonsoft.Json.Linq;
 using RestSharp;
 
-
 namespace SDETAPI_CSharp.Tests;
 
-//Second Pull Request 
-
+//Third Pull Request 
 public class Program
 {
+
     
-    public static void SecondPullHealthCg()
+    public static void ThirdPullNasaOpenApi()
     {   
-        string fileName = "C:/Users/carlos.martinez/source/repos/SDETAPI_CSharp/SDETAPI_CSharp/Requests/HealthcareGov/Gets/hcgGetRequest.json";
-        var body = JsonReader.readHCGJsonFile(fileName);
-    
-        IRestResponse response = RestCore.CreateRequestWithHeaders(body.Url, body.Method);
-        JObject content = JObject.Parse(response.Content.ToString());
-        
-        Console.WriteLine("\nStatus Code: " + response.StatusCode + "\n" +
-                          "\nContent: " + content);
-    }
-    
-    public static void SecondPullNasaOpenApi()
-    {   
-        string fileName = "C:/Users/carlos.martinez/source/repos/SDETAPI_CSharp/SDETAPI_CSharp/Requests/NasaOpenAPI/Gets/nasaGetRequest.json";
+        string fileName = "C:/Users/carlos.martinez/source/repos/SDET_API_CSharp/SDETAPI_CSharp/SDETAPI_CSharp/Requests/NasaOpenAPI/Gets/nasaGetRequest.json";
         var body = JsonReader.readNasaJsonFile(fileName);
     
-        IRestResponse response = RestCore.CreateRequestWithHeaders(body.Url, body.Method);
+        IRestResponse response = RestCore.CreateRequestWithStatus(body.Url, body.Method, body.status, body.statusCode);
         JObject content = JObject.Parse(response.Content.ToString());
         
-        if (!string.Equals(response.StatusCode.ToString(), "Ok", StringComparison.OrdinalIgnoreCase))
-            Console.WriteLine("\nError: "+ response.ErrorMessage);
-        else {
-            Console.WriteLine("\nStatus Code: " + response.StatusCode + "\n" +
-                              "\nContent: " + content);
-        }
-        
+        Console.WriteLine("\n\nContent: " + content);
         
     }
 }
+
